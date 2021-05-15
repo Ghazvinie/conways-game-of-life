7b3cd69f97a2b897803416327e00807a2774fd60
Daniel Ghazvinie <danielghazvinie@gmail.com>
2021-05-15 09:51:32 +0100

    added some more comments

--- gameOfLife.js
+++ gameOfLife.js
@@ -1,72 +1,259 @@
-const rows = 40;
-const columns = 40;
+const startButton = document.getElementById('startBtn');
+const resetButton = document.getElementById('reset');
+const gridSubmit = document.getElementById('submitGrid');
+const gridSize = document.getElementById('gridSize');
+
+// Default grid size
+let rows = 40;
+let columns = 40;
+
+// Is world running
+let running = false;
+
+// Generate custom grid
+gridSubmit.addEventListener('click', () => {
+    // Delete old grid
+    const world = document.getElementById('world');
+    const table = document.getElementById('worldGrid');
+    world.removeChild(table);
+    // Set arrays to empty
+    currentGeneration = [];
+    nextGeneration = [];
+    // Change grid size
+    rows = Number(gridSize.value);
+    columns = rows;
+  
+  // Generate new world and arrays
+  worldArray();
+  createWorld();
+  stop();
+  running = !running;
+});
+
+// Start world
+
+startButton.addEventListener('click', () => {
+    if (!running) {
+        life();
+        running = !running;
+    } else {
+        stop();
+        running = !running;
+    }
+});
+
+// Reset world
+resetButton.addEventListener('click', resetWorld);
 
 // World arrays
 let currentGeneration = [];
 let nextGeneration = [];
 
 // Initialise array representation of world for current and next generations
-(function worldArray() {
-
-    for (let i = 0 ; i < rows; i++){
+function worldArray() {
+    for (let i = 0; i < rows; i++) {
         currentGeneration[i] = new Array(rows);
         nextGeneration[i] = new Array(rows);
-        for (let j = 0; j < columns; j++){
+        for (let j = 0; j < columns; j++) {
             currentGeneration[i][j] = 0;
             nextGeneration[i][j] = 0;
         }
     }
-})();
+}
 
-//Create world/table
+// Reset world back to all dead
+function resetWorld() {
+    currentGeneration.map((row, rowIdx) => row.map((cell, cellIdx) => {
+        cell = document.getElementById(`${rowIdx}_${cellIdx}`);
+        cell.setAttribute('class', 'dead');
+        currentGeneration[rowIdx][cellIdx] = 0;
+        nextGeneration[rowIdx][cellIdx] = 0;
+    }));
+    stop();
+}
+
+
+let mousedown = false;
+// Create world/table
 function createWorld() {
     const world = document.getElementById('world');
     const table = document.createElement('table');
     table.setAttribute('id', 'worldGrid');
 
-    for (let i = 0; i < rows; i++) {
+    currentGeneration.map((row, rowIdx) => {
         let tableRow = document.createElement('table');
-        for (let j = 0; j < 40; j++) {
-            let cell = document.createElement('td');
-            cell.setAttribute('id', i + '_' + j);
-            cell.setAttribute('class', 'dead');
-            cell.addEventListener('click', startClick);
 
+        row.map((cell, cellIdx) => {
+            // Create table cells
+            cell = document.createElement('td');
+            cell.setAttribute('id', `${rowIdx}_${cellIdx}`);
+            cell.setAttribute('class', 'dead');
+
+            // Add cell event listeners
+            cell.addEventListener('click', selectCells);
+            cell.addEventListener('mouseover', function() {
+                if (mousedown) {
+                    selectCells.call(this);
+                }
+            });
+            // Add table event listeners to manage mouse up/down
+            table.addEventListener('mouseup', () => mousedown = false);
+            table.addEventListener('mousedown', () => mousedown = true);
+
+            // Add cell to row
             tableRow.appendChild(cell);
-        }
+        });
+        // Add row to table
         table.appendChild(tableRow);
-    }
+    });
+    // Add table to div
     world.appendChild(table);
 }
 
-// Click initial live cells
-function startClick(event) {
+// User sets initial live cells
+function selectCells(event) {
     const x = this.id.split('_')[0];
     const y = this.id.split('_')[1];
 
-    if (this.className === 'alive'){
+    if (this.className === 'alive') {
         this.setAttribute('class', 'dead');
         currentGeneration[x][y] = 0;
-
     } else {
         this.setAttribute('class', 'alive');
         currentGeneration[x][y] = 1;
     }
 }
 
+// Count number of live neighbours of cell
+function getNeighboursCount(x, y) {
+
+    // Top Left
+    if (x - 1 < 0 && y - 1 < 0) {
+    return [
+                                       [currentGeneration[x][y + 1]],
+        [currentGeneration[x + 1][y]], [currentGeneration[x + 1][y + 1]]
+       ].filter(cell => cell[0] === 1).length;
+    } else
+
+    // Top Right
+    if (x -1 < 0 && y + 1 > columns -1) {
+        return [
+            [currentGeneration[x][y - 1]],
+            [currentGeneration[x + 1][y - 1]], [currentGeneration[x + 1][y]]
+        ].filter(cell => cell[0] === 1).length;
+    } else 
+
+    // Bottom Left
+    if (x + 1 > rows -1 && y - 1 < 0) {
+        return [
+            [currentGeneration[x - 1][y]], [currentGeneration[x - 1][y + 1]],
+                                           [currentGeneration[x][y + 1]]
+        ].filter(cell => cell[0] === 1).length;
+    } else 
+
+    // Bottom Right
+    if (x + 1 > rows - 1 && y + 1 > columns - 1) {
+        return [
+            [currentGeneration[x - 1][y - 1]], [currentGeneration[x - 1][y]],
+            [currentGeneration[x][y - 1]],
+        ].filter(cell => cell[0] === 1).length;
+    } else 
+
+    // Top edge
+    if (x - 1 < 0) {
+        return [
+            [currentGeneration[x][y - 1]],                                    [currentGeneration[x][y + 1]],
+            [currentGeneration[x + 1][y - 1]], [currentGeneration[x + 1][y]], [currentGeneration[x + 1][y + 1]]
+        ].filter(cell => cell[0] === 1).length;
+    } else 
+
+    // Bottom edge
+    if (x + 1 > rows -1) {
+        return [
+            [currentGeneration[x - 1][y - 1]],    [currentGeneration[x -1][y]], [currentGeneration[x -1 ][y + 1]],
+            [currentGeneration[x][y - 1]],                                      [currentGeneration[x][y + 1]]
+        ].filter(cell => cell[0] === 1).length;
+    } else 
+
+    // Right edge
+    if (y + 1 > columns - 1){
+        return [
+            [currentGeneration[x - 1][y - 1]], [currentGeneration[x -1 ][y]],
+            [currentGeneration[x][y - 1]],
+            [currentGeneration[x + 1][y - 1]], [currentGeneration[x + 1][y]]
+        ].filter(cell => cell[0] === 1).length;
+    } else 
+
+    // Left edge
+    if (y - 1 < 0) {
+        return [
+            [currentGeneration[x - 1][y]], [currentGeneration[x -1 ][y + 1]],
+                                           [currentGeneration[x][y + 1]],
+            [currentGeneration[x + 1][y]], [currentGeneration[x + 1][y + 1]]
+        ].filter(cell => cell[0] === 1).length;
+    } else {
+
+    // Middle
+        return [
+            [currentGeneration[x - 1][y - 1]], [currentGeneration[x - 1][y]], [currentGeneration[x - 1][y + 1]],
+            [currentGeneration[x][y - 1]],                                    [currentGeneration[x][y + 1]],
+            [currentGeneration[x + 1][y -1]],[currentGeneration[x + 1][y]],   [currentGeneration[x + 1][y + 1]]
+        ].filter(cell => cell[0] === 1).length;
+    }
+}
+
+// Create the next generation
+function createNext(){
+    currentGeneration.map((row, rowIdx) => row.map((cell, cellIdx) => {
+        let liveNeighbours = getNeighboursCount(rowIdx, cellIdx);
+            if ((liveNeighbours < 2 || liveNeighbours > 3) && currentGeneration[rowIdx][cellIdx] === 1){
+                nextGeneration[rowIdx][cellIdx] = 0;
+            } else if (liveNeighbours === 3 && currentGeneration[rowIdx][cellIdx] === 0) {
+                nextGeneration[rowIdx][cellIdx] = 1;
+            } else {
+                nextGeneration[rowIdx][cellIdx] = currentGeneration[rowIdx][cellIdx];
+            }
+    }));
+}
+
+// Update current generation to be next generation just generated
+function updateCurrent(){
+    currentGeneration.map((row, rowIdx) => row.map((cell, cellIdx) => {
+        currentGeneration[rowIdx][cellIdx] = nextGeneration[rowIdx][cellIdx];
+        nextGeneration[rowIdx][cellIdx] = 0;
+    }));
+}
+
+// Update html world so it matches current generation array
+function updateWorld() {
+    let cell = '';
+    currentGeneration.map((row, rowIdx) => row.map((cell, cellIdx) => {
+        cell = document.getElementById(`${rowIdx}_${cellIdx}`);
+            if (currentGeneration[rowIdx][cellIdx] === 1){
+                cell.setAttribute('class', 'alive');
+            } else if (currentGeneration[rowIdx][cellIdx] === 0){
+                cell.setAttribute('class', 'dead');
+            }
+    }));
+}
+
+// Start life
+let interval;
+function life(){
+    interval = setInterval(() => {
+        createNext();
+        updateCurrent();
+        updateWorld();
+    },100);
+}
+
+// Pause life 
+function stop () {
+    clearInterval(interval);
+}
+
+// Create an initial world and arrays 
 window.onload = () => {
+    worldArray();
     createWorld();
-  };
-
-console.log(currentGeneration);
-
-
-
-
-
-
-
-
-
-
-
+};
